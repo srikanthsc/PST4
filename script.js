ff07376fc32ef426cc70d9437c97858fdb626800
srikanthsc <79998871+srikanthsc@users.noreply.github.com>
2021-12-29 16:30:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
+
+$( '.friend-drawer--onhover' ).on( 'click',  function() {
+  
+  $( '.chat-bubble' ).hide('slow').show('slow');
+  
+});
+
