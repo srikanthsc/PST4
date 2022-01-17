0b1b622ddde9122a98084179fb4e4d3794361bb5
Najmi19 <80950281+Najmi19@users.noreply.github.com>
2022-01-17 11:13:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
-
-$( '.friend-drawer--onhover' ).on( 'click',  function() {
-  
-  $( '.chat-bubble' ).hide('slow').show('slow');
-  
-});
-
